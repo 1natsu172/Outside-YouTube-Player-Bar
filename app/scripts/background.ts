7812517dce24d4eefb403fbdc28c250b6935474c
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-07 21:21:15 +0900

    init

---
+++ app/scripts/background.ts
@@ -0,0 +1,12 @@
+// Enable chromereload by uncommenting this line:
+// import 'chromereload/devonly'
+
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.tabs.onUpdated.addListener((tabId) => {
+  chrome.pageAction.show(tabId);
+});
+
+console.log(`'Allo 'Allo! Event Page for Page Action`);
