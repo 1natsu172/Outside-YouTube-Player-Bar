dc07e878c6c3738a1839b6fca70d95ec72a5127a
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-21 22:50:43 +0900

    add WaitElement Utility

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,4 +1,12 @@
-import barHeight from './libs/getPlayerBarHeight'
-console.log(barHeight)
+const WaitElement = require('./libs/waitElement')
+import BarHeight from './libs/getPlayerBarHeight'
+console.log(BarHeight)
+
+const getcontainer = async () => {
+  const observeTarget = document.body
+  const wait = await WaitElement(observeTarget, '#player-container')
+  console.log(wait)
+}
+getcontainer()
 
 // // `<style>:root{--oypb-player-bar-height: ${playerBarHeight};}</style>`
