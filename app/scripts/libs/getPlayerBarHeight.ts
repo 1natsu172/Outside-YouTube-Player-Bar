f5d80aa26ba3233b746b5020add91f61a4ec25d5
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-21 15:25:51 +0900

    add getPlayerBar component

---
+++ app/scripts/libs/getPlayerBarHeight.ts
@@ -0,0 +1,4 @@
+const playerBar = document.querySelector('.ytp-chrome-bottom') as HTMLElement
+const playerBarHeight: number | null = playerBar.clientHeight
+
+export default playerBarHeight
---
+++ app/scripts/main.ts
@@ -0,0 +1,4 @@
+import barHeight from './libs/getPlayerBarHeight'
+console.log(barHeight)
+
+// // `<style>:root{--oypb-player-bar-height: ${playerBarHeight};}</style>`
