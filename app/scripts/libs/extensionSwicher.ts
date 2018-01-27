c929f4ad926729631a02c6fb71ab8ffd5d131b28
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-27 18:16:03 +0900

    change extension enabled class

--- app/scripts/libs/extensionSwicher.ts
+++ app/scripts/libs/extensionSwicher.ts
@@ -6,27 +6,27 @@ const ExtensionSwitcherWrapper = class ExtensionSwitcherWrapper {
   }
 
   action = (): void => {
-    document.body.classList.add('is-outsided-playerBar')
+    document.body.classList.add('oypb-is-outsided-playerBar')
     this._isExtensionActioning = true
     console.log('is?', this._isExtensionActioning)
   }
 
   stop = (): void => {
-    document.body.classList.remove('is-outsided-playerBar')
+    document.body.classList.remove('oypb-is-outsided-playerBar')
     this._isExtensionActioning = false
     console.log('is?', this._isExtensionActioning)
   }
 
   pause = (): void => {
     if (this._isExtensionActioning) {
-      document.body.classList.toggle('is-outsided-playerBar')
+      document.body.classList.toggle('oypb-is-outsided-playerBar')
       console.log('is?', 'pause')
     }
   }
 
   toggle = (): void => {
     console.log('is?', this._isExtensionActioning)
-    document.body.classList.toggle('is-outsided-playerBar')
+    document.body.classList.toggle('oypb-is-outsided-playerBar')
     this._isExtensionActioning = this._isExtensionActioning ? false : true
     console.log('is?', this._isExtensionActioning)
   }
