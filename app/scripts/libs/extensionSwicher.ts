d19eb2476047115bdc3bae8446c6ceb93feec5c5
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-28 18:38:43 +0900

    'outsided' is typo...

--- app/scripts/libs/extensionSwicher.ts
+++ app/scripts/libs/extensionSwicher.ts
@@ -6,27 +6,27 @@ const ExtensionSwitcherWrapper = class ExtensionSwitcherWrapper {
   }
 
   active = (): void => {
-    document.body.classList.add('oypb-is-outsided-playerBar')
+    document.body.classList.add('oypb-is-outside-playerBar')
     this._isExtensionActioning = true
     console.log('is?', this._isExtensionActioning)
   }
 
   inactive = (): void => {
-    document.body.classList.remove('oypb-is-outsided-playerBar')
+    document.body.classList.remove('oypb-is-outside-playerBar')
     this._isExtensionActioning = false
     console.log('is?', this._isExtensionActioning)
   }
 
   pause = (): void => {
     if (this._isExtensionActioning) {
-      document.body.classList.toggle('oypb-is-outsided-playerBar')
+      document.body.classList.toggle('oypb-is-outside-playerBar')
       console.log('is?', 'pause')
     }
   }
 
   toggle = (): void => {
     console.log('is?', this._isExtensionActioning)
-    document.body.classList.toggle('oypb-is-outsided-playerBar')
+    document.body.classList.toggle('oypb-is-outside-playerBar')
     this._isExtensionActioning = this._isExtensionActioning ? false : true
     console.log('is?', this._isExtensionActioning)
   }
