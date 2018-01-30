7c2b612e57d3ac46c35bc2ba678ede7b04830529
1natsu172 <1natsu172@users.noreply.github.com>
2018-01-31 00:14:17 +0900

    implement initial toopltip state

--- app/scripts/actions/extensionButton.ts
+++ app/scripts/actions/extensionButton.ts
@@ -1,21 +1,25 @@
 const waitElement = require('../libs/waitElement')
-import extension from '../libs/extensionSwicher'
+import extension from './extensionSwicher'
+import state from '../libs/stateMap'
 import toggleTooltip from '../libs/toggleTooltip'
 
 const tooltipText = {
-  active: chrome.i18n.getMessage('tooltipText_active') as string,
-  inactive: chrome.i18n.getMessage('tooltipText_inactive') as string
+  isActive: chrome.i18n.getMessage('tooltipText_isActive') as string,
+  isInactive: chrome.i18n.getMessage('tooltipText_isInactive') as string
 }
 
-const initialTooltipText = () => {
-  if (extension.isExtensionActioning) {
-    return tooltipText.inactive
+const initialTooltipText = (): string => {
+  console.log('init-toopltip-hasinject?', state.get('hasInjected'))
+
+  if (!state.get('hasInjected')) {
+    return tooltipText.isActive
   } else {
-    return tooltipText.active
+    console.log(state.get('isActive'))
+    return state.get('isActive') ? tooltipText.isActive : tooltipText.isInactive
   }
 }
 
-const extensionToggleButton = `
+const extensionToggleButton = (): string => `
   <button id="oypb-toggleExtension" class="ytp-button oypb-toggleExtensionButton oypb-tooltip" data-oypb-tooltip="${initialTooltipText()}">
     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 18 16">
       <path id="oypb-toggle" fill="#fff" d="M0 0h18v5H0zm6.78 5v5.39H3.39L9 16l5.61-5.61h-3.39V5H6.78z"/>
@@ -23,28 +27,29 @@ const extensionToggleButton = `
   </button>
 `
 
-const setToggleExtensionEvent = () => {
+const setExtensionButtonEvent = (): void => {
   const button = document.getElementById('oypb-toggleExtension') as HTMLElement
 
   button.addEventListener('click', extension.toggle)
   button.addEventListener('click', e =>
-    toggleTooltip(e, tooltipText.active, tooltipText.inactive)
+    toggleTooltip(e, tooltipText.isActive, tooltipText.isInactive)
   )
 }
 
-export const injectButton = async () => {
+export const injectButton = async (): Promise<void> => {
   // Wait target node.
   const playerBar = document.querySelector('.ytp-chrome-bottom')
   const rightControls = await waitElement(playerBar, '.ytp-right-controls')
+  const button = extensionToggleButton()
 
   // Inject button dom.
-  rightControls.insertAdjacentHTML('afterbegin', extensionToggleButton)
+  rightControls.insertAdjacentHTML('afterbegin', button)
 
   // Set Events.
-  setToggleExtensionEvent()
+  setExtensionButtonEvent()
 }
 
-export const removeButton = () => {
+export const removeButton = (): void => {
   const button = document.getElementById('oypb-toggleExtension')
   if (button) button.remove()
 }
