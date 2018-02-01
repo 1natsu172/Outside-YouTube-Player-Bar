f5b2352bdf60319d43f8a11e4b329eb972a08b9d
1natsu172 <1natsu172@users.noreply.github.com>
2018-02-01 22:08:20 +0900

    :bento: Add gtag(analytics)
    gtag.ts

---
+++ app/scripts/libs/gtag.ts
@@ -0,0 +1,11 @@
+export default `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-105352326-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-105352326-2');
+</script>
+`
