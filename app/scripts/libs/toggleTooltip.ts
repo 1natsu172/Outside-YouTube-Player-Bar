c16ceab7f2d902e07ae126f9267754d6a2b29985
1natsu172 <1natsu172@users.noreply.github.com>
2018-02-07 00:32:29 +0900

    lite modify to argument element

--- app/scripts/libs/toggleTooltip.ts
+++ app/scripts/libs/toggleTooltip.ts
@@ -1,5 +1,5 @@
 export default function toggleTooltipText(e: any, f: string, s: string) {
-  const element: HTMLElement = e.target || this
+  const element: HTMLElement = e.target || e || this
   const firstText = f
   const secondText = s
   const currentText = element.getAttribute('data-oypb-tooltip')
