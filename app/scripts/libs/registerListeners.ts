dbde2e3e1ee28ae7519ffb9b7b13f3412e3be632
1natsu172 <1natsu172@users.noreply.github.com>
2019-02-15 00:04:09 +0900

    🎨 refactor: tweak eventlisteners functions
    separate to registerListeners.ts

---
+++ app/scripts/libs/registerListeners.ts
@@ -0,0 +1,68 @@
+import extension from '../actions/extensionSwicher'
+
+const YT_EVENTS = [
+  'yt-request-panel-mode-change',
+  'yt-service-request-completed',
+  'yt-navigate',
+  'yt-navigate-start',
+  'yt-navigate-finish',
+  'yt-page-data-updated',
+  'yt-navigate-set-page-offset',
+  'visibilitychange',
+  'yt-autonav-pause-focus',
+  'yt-visibility-refresh',
+  'yt-page-data-will-update',
+  'yt-page-manager-navigate-start',
+  'yt-navigate-start'
+]
+
+const DEBUG_YT_EVENTS = () =>
+  YT_EVENTS.forEach(eventName => {
+    document.addEventListener(eventName, e => {
+      console.log(eventName, e)
+    })
+  })
+
+const pageNavigateListener = () => {
+  document.addEventListener('yt-navigate-finish', e => {
+    console.log(e)
+    console.log('PAGE NAVIGATING...')
+    extension.inactive()
+  })
+}
+
+const videoLoadedListener = () => {
+  document.addEventListener('yt-page-data-updated', e => {
+    console.log(e)
+    console.log('PAGE UPDATED')
+    extension.active()
+  })
+}
+
+const fullscreenListener = () => {
+  const events = [
+    'fullscreenchange',
+    'webkitfullscreenchange',
+    'mozfullscreenchange',
+    'MSFullscreenChange'
+  ]
+  events.forEach(event => {
+    // Pause the extension when fullscreen mode.
+    document.addEventListener(event, () => {
+      const extensionButton = document.getElementById(
+        'oypb-toggleExtension'
+      ) as HTMLElement
+
+      extensionButton.classList.toggle('oypb-is-none')
+      extension.pause()
+    })
+  })
+}
+
+const registerListeners = (): void => {
+  pageNavigateListener()
+  videoLoadedListener()
+  // fullscreenListener()
+}
+
+export { registerListeners, DEBUG_YT_EVENTS }
