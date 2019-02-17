a714f042c8ec2a33da675cb45e15ab035c5b5aef
1natsu172 <1natsu172@users.noreply.github.com>
2019-02-17 17:35:43 +0900

    🎨 feat: implement observeNodes & isVisiblePlayerBar

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,6 +1,7 @@
 import extension from './actions/extensionSwicher'
 import state from './libs/stateMap'
 import { DEBUG_YT_EVENTS, registerListeners } from './libs/registerListeners'
+import { observeNodes } from './actions/observeNodes'
 
 const IS_DEBUG_YT_EVENTS = false
 
@@ -11,4 +12,5 @@ const initExtension = (() => {
   state.set('hasInjected', false) // set init flag
   console.log('Init: hasInject?', state.get('hasInjected'))
   registerListeners()
+  observeNodes()
 })()
