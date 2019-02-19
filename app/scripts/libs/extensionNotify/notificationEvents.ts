ab04d78c69e8c07d54febf63dc18423a834cb3be
1natsu172 <1natsu172@users.noreply.github.com>
2019-02-20 00:10:35 +0900

    fix: onClick doen't work in notification center

--- app/scripts/libs/extensionNotify/notificationEvents.ts
+++ app/scripts/libs/extensionNotify/notificationEvents.ts
@@ -2,9 +2,12 @@ const CHANGELOG_URL =
   'https://github.com/1natsu172/Outside-YouTube-Player-Bar/blob/master/CHANGELOG.md'
 
 export const onClickNotification = (id: string) => {
-  chrome.notifications.onClicked.addListener(notificationId => {
+  const onClickCallback = (notificationId: string) => {
     if (notificationId === id) {
       window.open(CHANGELOG_URL, '_blank')
     }
-  })
+  }
+
+  chrome.notifications.onClicked.addListener(onClickCallback)
+  chrome.notifications.onButtonClicked.addListener(onClickCallback)
 }
