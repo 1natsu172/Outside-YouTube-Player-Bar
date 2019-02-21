2923d4b621de880414e823908ed19bd39ed926e2
1natsu172 <1natsu172@users.noreply.github.com>
2019-02-22 00:16:25 +0900

    build: remove unnecessary tasks

--- tasks/pack.js
+++ tasks/pack.js
@@ -1,31 +1,6 @@
-import gulp from 'gulp'
-import { colors, log } from 'gulp-util'
-import zip from 'gulp-zip'
-import packageDetails from '../package.json'
-import args from './lib/args'
+const shell = require('shelljs')
+const version = require('../dist/manifest.json').version
 
-function getPackFileType () {
-  switch (args.vendor) {
-    case 'firefox':
-      return '.xpi'
-    case 'opera':
-      return '.crx'
-    default:
-      return '.zip'
-  }
-}
+const fileName = `Outside-YouTube-Player-Bar-${version}`
 
-gulp.task('pack', ['build'], () => {
-  let name = packageDetails.name
-  let version = packageDetails.version
-  let filetype = getPackFileType()
-  let filename = `${name}-${version}-${args.vendor}${filetype}`
-  return gulp.src(`dist/${args.vendor}/**/*`)
-    .pipe(zip(filename))
-    .pipe(gulp.dest('./packages'))
-    .on('end', () => {
-      let distStyled = colors.magenta(`dist/${args.vendor}`)
-      let filenameStyled = colors.magenta(`./packages/${filename}`)
-      log(`Packed ${distStyled} to ${filenameStyled}`)
-    })
-})
+shell.exec(`zip -9 -r packages/${fileName}.zip dist/*`)
