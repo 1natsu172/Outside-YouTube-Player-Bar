8a9626b4030fcb306953f48e5eac66734186a970
1natsu <1natsu172@users.noreply.github.com>
2022-07-11 20:48:38 +0900

    chore: note

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,8 @@
-import style from '../styles/style.scss'
+// NOTE: viteがmanifestからのasset処理をせずJSからの参照がないとdistにコンパイルして吐いてくれないので、scssをsideEffect importしている
+import '../styles/style.scss'
 import { DEBUG_YT_EVENTS, registerListeners } from './actions/registerListeners'
 import { observeNodes } from './actions/observeNodes'
 import { initialize } from './actions/inittialize'
-
-console.log(style)
 
 const IS_DEBUG_YT_EVENTS = false
 
