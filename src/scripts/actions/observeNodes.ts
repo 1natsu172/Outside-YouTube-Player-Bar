c17d103e0de9a44b6af9197f4b2405e4b6b4820e
1natsu <1natsu172@users.noreply.github.com>
2024-04-05 16:11:46 +0900

    feat: dynamic player bar height

--- src/scripts/actions/observeNodes.ts
+++ src/scripts/actions/observeNodes.ts
@@ -1,14 +1,24 @@
 import { waitElement } from '@1natsu/wait-element'
 import { debounce } from 'mabiki'
 import { observeIsVisiblePlayerBar } from '../controllers/observeIsVisiblePlayerBar'
+import { observeHeightOfPlayerBar } from '../controllers/observeHeightOfPlayerBar.js'
 import { displayPlayerBar } from '../controllers/displayPlayerBar.js'
+import { setPlayerBarHeight } from '../usecases/setCssVariables.js'
 
 export const observeNodes = async () => {
   const player = await waitElement('#movie_player')
   const { blockAutohide } = await displayPlayerBar()
+  const playerBarContainer = await waitElement('.ytp-chrome-bottom')
 
   observeIsVisiblePlayerBar(player as Node, {
     blockAutohide: debounce(blockAutohide, 1000, {
+      trailing: true,
+      leading: true,
+    }),
+  })
+
+  observeHeightOfPlayerBar(playerBarContainer, {
+    setPlayerBarHeight: debounce(setPlayerBarHeight, 500, {
       trailing: true,
       leading: true,
     }),
