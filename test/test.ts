04573a82b55f6b27560d2767f501c0ef0039a738
1natsu <1natsu172@users.noreply.github.com>
2024-04-05 19:25:43 +0900

    chore: tweak deps and clean files (#68)

--- test/test.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-
-test('Insert to DOM', t => {
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-
-  t.is(document.querySelector('div'), div)
-})
