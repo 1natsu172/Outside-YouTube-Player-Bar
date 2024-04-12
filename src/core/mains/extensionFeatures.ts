3b75576c630a33ce96adb6adfe95cfb267244b8d
1natsu <1natsu172@users.noreply.github.com>
2024-04-13 00:50:53 +0900

    option model

---
+++ src/core/mains/extensionFeatures.ts
@@ -0,0 +1,4 @@
+export type ExtensionBehavior = {
+  positionPlayerBar: 'inside' | 'outside'
+  alwaysDisplayPlayerBar: boolean
+}
--- src/core/mains/options.ts
+++ src/core/mains/options.ts
@@ -1,16 +1,27 @@
 import type { StorageArea } from '@/core/infrastructures/storage/constants.js'
+import type { ExtensionBehavior } from '@/core/mains/extensionFeatures.js'
 
-type OptionV<V> = {
+type OptionType<
+  V,
+  M extends Record<string, unknown> = Record<string, never>,
+> = {
   storageArea: StorageArea
   value: V
+  meta: M
 }
 
 export interface ExtensionMetaOptions {
-  debugMode: OptionV<boolean>
-  forceDisableForEscapeHatch: boolean
+  debugModeV1: OptionType<boolean>
+  forceDisableV1: OptionType<
+    boolean,
+    {
+      extensionVersion: string
+    }
+  >
 }
 
 export interface UserOptions {
-  fullscreenBehavior: {}
-  defaultBehavior: {}
+  defaultViewBehaviorV1: OptionType<ExtensionBehavior>
+  theaterModeBehaviorV1: OptionType<ExtensionBehavior>
+  fullscreenBehaviorV1: OptionType<ExtensionBehavior>
 }
