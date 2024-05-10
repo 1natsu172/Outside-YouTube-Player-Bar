9fe9900ef96138db24fc15e1fd239d1baf9370ea
1natsu <1natsu172@users.noreply.github.com>
2024-05-11 03:42:38 +0900

    forceDisableパネルだけ置く

--- src/sharedUI/Components/forms/SettingsForm/FormDefinition/ExtensionMetaOptions.definition.tsx
+++ src/sharedUI/Components/forms/SettingsForm/FormDefinition/ExtensionMetaOptions.definition.tsx
@@ -8,12 +8,61 @@ import { FormGroup } from "../FormGroup.js";
 import { FormField } from "../FormField.js";
 import { AutoSaveForFormField } from "../../utils/useAutoSaveForm.js";
 import type { FormDefs } from "./formDefinition.types.js";
-import { getDebugModeOption } from "@/core/presenters/storagePresenter/options.presenter.js";
+import {
+	getDebugModeOption,
+	getForceDisableOption,
+} from "@/core/presenters/storagePresenter/options.presenter.js";
+import { setForceDisableOption } from "@/core/usecases/options.usecase.js";
 
 /**
  * @description Enumerate component implementations
  */
 export const ExtensionMetaOptionsFormDefs: FormDefs = new Map([
+	[
+		"Form:ForceDisable",
+		{
+			FormElement: ({ formId }) => {
+				const queryKey = [formId];
+
+				return (
+					<FormGroup title="">
+						<AutoSaveForFormField
+							option={{
+								useSuspenseQueryArgs: [
+									{ queryKey, queryFn: getForceDisableOption },
+								],
+								useMutationArgs: [{ mutationFn: setForceDisableOption }],
+							}}
+						>
+							{([{ data, isLoading }, { mutate, isPending }]) => (
+								<FormField
+									title={browser.i18n.getMessage(
+										"settings_metaOption_forceDisable_title",
+									)}
+									description={browser.i18n.getMessage(
+										"settings_metaOption_forceDisable_description",
+									)}
+									isLoading={isPending}
+									formState={{ data }}
+								>
+									<Switch
+										size="lg"
+										checked={data}
+										onChange={(e) => {
+											mutate(e.target.checked);
+										}}
+										offLabel="OFF"
+										onLabel="ON"
+										disabled={isLoading || isPending}
+									/>
+								</FormField>
+							)}
+						</AutoSaveForFormField>
+					</FormGroup>
+				);
+			},
+		},
+	],
 	[
 		"Form:DebugMode",
 		{
