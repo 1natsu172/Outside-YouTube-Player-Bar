efd1336a5a153a5816eb46ef99f52f383fc0f9a8
1natsu <1natsu172@users.noreply.github.com>
2024-05-16 01:17:53 +0900

    re

--- wxt.config.ts
+++ wxt.config.ts
@@ -25,7 +25,7 @@ export const manifestJsonConfig: UserConfig["manifest"] = (_configEnv) => ({
 	short_name: "__MSG_appShortName__",
 	description: "__MSG_appDescription__",
 	// TODO: リリース前に消す（package.jsonから自動生成っぽい）
-	version: "4.0.2",
+	version: "4.0.0",
 	// NOTE: Firefox is still mv2, so not explicitly. https://wxt.dev/guide/multiple-browsers.html#target-manifest-version
 	// manifest_version: 3,
 	default_locale: "en",
