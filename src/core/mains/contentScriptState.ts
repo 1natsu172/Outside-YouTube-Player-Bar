f5f9f1579e0a31cfec79b98a96a0ac1345c48e69
1natsu <1natsu172@users.noreply.github.com>
2024-05-20 16:47:55 +0900

    tw

--- src/core/mains/contentScriptState.ts
+++ src/core/mains/contentScriptState.ts
@@ -62,8 +62,6 @@ export type ContentScriptState = {
 	/**
 	 * This is "Current" state. Not globally behavior state(user-option storage content).
 	 * If the user changes the user-option, this state should be sync to that state.
-	 * @todo:
-	 * When the player mode changes, user can set the option to force the behavior of the default option.
 	 */
 	behavior: BehaviorState;
 	// TODO: storageの状態をcontent-script内にreactiveに持ち込む必要がどうしても出たらUserOptionをメモリストア上に同期させる必要がある 普通はstorageにgetValueすればいいはずだが…。
