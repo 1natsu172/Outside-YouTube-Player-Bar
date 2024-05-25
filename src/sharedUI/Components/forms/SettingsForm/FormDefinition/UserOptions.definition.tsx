f4b1bdf0d419b152230e468d6d7b3e7c9f73c915
1natsu <1natsu172@users.noreply.github.com>
2024-05-25 23:45:56 +0900

    refactor: ShowOpenSettingsIconForm

--- src/sharedUI/Components/forms/SettingsForm/FormDefinition/UserOptions.definition.tsx
+++ src/sharedUI/Components/forms/SettingsForm/FormDefinition/UserOptions.definition.tsx
@@ -1,11 +1,6 @@
 import { videoPlayerModeKeysWithoutNone } from "@/core/presenters/statePresenter/siteMetaState/index.js";
-import { getShowOpenSettingsIconOption } from "@/core/presenters/storagePresenter/options.presenter.js";
-import { setShowOpenSettingsIconOption } from "@/core/services/optionsServices/userOptions.service.js";
-import { Switch } from "@mantine/core";
-import { FormField } from "../../layouts/FormField.js";
-import { FormGroup } from "../../layouts/FormGroup.js";
-import { AutoSaveForFormField } from "../../utils/useAutoSaveForm.js";
 import { ExtensionBehaviorForm } from "./ExtensionBehaviorForm/index.js";
+import { ShowOpenSettingsIconForm } from "./ShowOpenSettingsIconForm/index.js";
 import type { FormDefs } from "./formDefinition.types.js";
 
 /**
@@ -29,51 +24,13 @@ export const ExtensionBehaviorOptionsFormDefs = new Map(
  * @description Enumerate component implementations
  * About the UIEnhanceOptions
  */
-export const UiEnhanceOptionsFormDefs: FormDefs = new Map([
+export const UiEnhanceOptionsFormDefs = new Map([
 	[
 		"Form:ShowOpenSettingsIcon",
 		{
-			FormElement: ({ formId }) => {
-				const queryKey = [formId];
-				return (
-					<FormGroup title="">
-						<AutoSaveForFormField
-							option={{
-								useSuspenseQueryArgs: [
-									{ queryKey, queryFn: getShowOpenSettingsIconOption },
-								],
-								useMutationArgs: [
-									{ mutationFn: setShowOpenSettingsIconOption },
-								],
-							}}
-						>
-							{([{ data, isLoading }, { mutate, isPending }]) => (
-								<FormField
-									title={browser.i18n.getMessage(
-										"settings_userOption_showOpenSettingsIcon_title",
-									)}
-									description={browser.i18n.getMessage(
-										"settings_userOption_showOpenSettingsIcon_description",
-									)}
-									isLoading={isPending}
-									formState={data}
-								>
-									<Switch
-										size="lg"
-										checked={data}
-										onChange={(e) => {
-											mutate(e.target.checked);
-										}}
-										offLabel="OFF"
-										onLabel="ON"
-										disabled={isLoading || isPending}
-									/>
-								</FormField>
-							)}
-						</AutoSaveForFormField>
-					</FormGroup>
-				);
+			FormElement: () => {
+				return <ShowOpenSettingsIconForm />;
 			},
 		},
 	],
-]);
+]) satisfies FormDefs;
