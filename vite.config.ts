1096a378fe58f42144dc9a20c1cae0e1ed32bbaf
1natsu <1natsu172@users.noreply.github.com>
2024-05-27 03:19:27 +0900

    fix: sourcemap broken issue
    Error when using sourcemap for reporting an error: Can't resolve original location of error.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import react from "@vitejs/plugin-react";
-import reactSvgr from "vite-plugin-svgr";
-import postcssNesting from "postcss-nesting";
 import postcssImport from "postcss-import";
+import postcssNesting from "postcss-nesting";
+// NOTE(FIXME): rollupが`use client`接頭辞を利用しているnode_modulesを解釈しきれずsourcemapが壊れる問題を対処するために入れている。tanstack-queryとmantineあたりが該当モジュール。 https://github.com/vitejs/vite/issues/15012#issuecomment-2049888711
+import preserveDirectives from "rollup-preserve-directives";
+import reactSvgr from "vite-plugin-svgr";
 // NOTE: WXTがviteをラップしているのでviteのdefineConfigの型を引っ張るためだけにwxtからimportしている（config置き場を分置しておきたいがため）
 import type { WxtViteConfig } from "wxt";
 
 export default {
-	plugins: [react(), reactSvgr()],
+	plugins: [react(), reactSvgr(), preserveDirectives()],
 	css: {
 		postcss: {
 			plugins: [postcssNesting, postcssImport()],
