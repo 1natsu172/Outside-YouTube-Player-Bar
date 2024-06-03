2de97ee34440195173620041117d1f022d969f17
1natsu <1natsu172@users.noreply.github.com>
2024-06-03 23:02:14 +0900

    fleeze: remove v3 unnecessary files

--- src/core/interfaces/oypbClassNames/index.ts
+++
@@ -1,6 +0,0 @@
-// TODO: もういらないかも
-export const oypbClassNames = {
-	"IS-VISIBLE-PLAYERBAR": "oypb-is-visible-playerBar",
-	"IS-OUTSIDE-PLAYERBAR": "oypb-is-outside-playerBar",
-	"IS-FULLSCREEN": "oypb-is-fullscreen",
-} as const;
--- src/core/services/enforceServices/stateConsistency.service.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description Manager for consistency between central storage and states.
- */
-export const stateConsistencyManager = () => {}; // TODO: valtioとつなぐ reactiveに状態に一貫性をもたせる実装。
-// import { behaviorState } from "@/core/repositories/contentScript.repository.js";
-
-// const storageValues = derive({
-// 	userOption: (get) => repo.getValue(),
-// });
