2f6caa69123b3e9440c6ce870ec4819700f0fcfd
1natsu <1natsu172@users.noreply.github.com>
2024-06-11 23:48:24 +0900

    e2e: hack evaluate module

--- e2e/flightUtils/youtube.ts
+++ e2e/flightUtils/youtube.ts
@@ -1,7 +1,12 @@
+import type { VideoPlayerModeWithoutNone } from "@/core/mains/contentScriptState.js";
 import {
 	elementQuery,
 	extensionNameCustomElementName,
 } from "@/core/mains/meta.js";
+import {
+	convertAttrToVideoPlayerMode,
+	judgeCurrentVideoPlayerMode,
+} from "@/core/presenters/statePresenter/siteMetaState/index.js";
 import type { Page } from "@playwright/test";
 import { URLS } from "../fixture.js";
 
@@ -22,11 +27,40 @@ export async function openYouTube(
 			// NOTE: mute for testing. ref(mute option not provide yet): https://github.com/microsoft/playwright/issues/19534
 			_.getMuteButton().click();
 		},
+		getYTDPageManager: () => page.locator(elementQuery.YTD_PAGE_MANAGER),
 		getMoviePlayer: () => page.locator(elementQuery.MOVIE_PLAYER),
 		getPlayerBar: () => page.locator(elementQuery.PLAYER_BAR),
 		getOypbButton: () => page.locator(extensionNameCustomElementName),
 		getOpenSettingsButton: () => page.locator(extensionNameCustomElementName),
 		getMuteButton: () => page.locator(".ytp-mute-button"),
+		getDefaultViewButton: () =>
+			page.locator(`.ytp-size-button[data-title-no-tooltip="Default view"]`),
+		getTheaterModeButton: () =>
+			page.locator(`.ytp-size-button[data-title-no-tooltip="Theater mode"]`),
+		getFullScreenButton: () => page.locator(".ytp-fullscreen-button"),
+		getTargetVideoPlayerModeButton: async (
+			targetMode: VideoPlayerModeWithoutNone,
+		) => {
+			const managerElement = await _.getYTDPageManager();
+			// NOTE: ブラウザ環境で実行されるのでimportしたモジュール評価ができないので、stringで渡すと内部でevalしてくれる。引数がElementだけでいいのでここでは自分でeval()していないだけ。https://playwright.dev/docs/evaluating
+			const currentMode = (await managerElement.evaluate(
+				judgeCurrentVideoPlayerMode.toString(),
+			)) as ReturnType<typeof judgeCurrentVideoPlayerMode>;
+			const convertedCurrentMode = convertAttrToVideoPlayerMode(currentMode);
+
+			if (targetMode === convertedCurrentMode) {
+				return null;
+			}
+
+			switch (targetMode) {
+				case "defaultView":
+					return _.getDefaultViewButton();
+				case "theaterMode":
+					return _.getTheaterModeButton();
+				case "fullscreen":
+					return _.getFullScreenButton();
+			}
+		},
 	};
 
 	await page.goto(url);
--- e2e/scenarios/extensionBehavior/alwaysDisplayPlayerBar.test.ts
+++ e2e/scenarios/extensionBehavior/alwaysDisplayPlayerBar.test.ts
@@ -7,60 +7,76 @@ import {
 } from "@@/e2e/flightUtils/waitFor.js";
 import { openYouTube } from "@@/e2e/flightUtils/youtube.js";
 
-test.describe("defaultView", () => {
-	const videoPlayerMode: VideoPlayerModeWithoutNone = "defaultView";
+const videoPlayerModes: VideoPlayerModeWithoutNone[] = [
+	"defaultView",
+	"theaterMode",
+	"fullscreen",
+];
 
-	test("enable: alwaysDisplayPlayerBar", async ({ page, extensionId }) => {
-		const { getAlwaysDisplayPlayerBar } = await openOptions(page, {
-			extensionId,
-			videoPlayerMode,
+for (const videoPlayerMode of videoPlayerModes) {
+	test.describe(videoPlayerMode, () => {
+		test("enable: alwaysDisplayPlayerBar", async ({ page, extensionId }) => {
+			const { getAlwaysDisplayPlayerBar } = await openOptions(page, {
+				extensionId,
+				videoPlayerMode,
+			});
+
+			const setting = await getAlwaysDisplayPlayerBar();
+
+			await expect(setting.input).toBeChecked();
+
+			const { playVideoPlayer, getPlayerBar, getTargetVideoPlayerModeButton } =
+				await openYouTube(page, {
+					url: URLS.sampleVideo,
+				});
+
+			await playVideoPlayer();
+
+			await waitForCumulativeLayoutShift(page);
+
+			const changeToTargetVideoMode =
+				await getTargetVideoPlayerModeButton(videoPlayerMode);
+			await changeToTargetVideoMode?.click();
+
+			await waitForElementAfterTimeout({
+				page,
+				locator: getPlayerBar(),
+				expect,
+				expectType: "visible",
+				timeout: 5000,
+			});
 		});
 
-		const setting = await getAlwaysDisplayPlayerBar();
+		test("disable: alwaysDisplayPlayerBar", async ({ page, extensionId }) => {
+			const { getAlwaysDisplayPlayerBar } = await openOptions(page, {
+				extensionId,
+				videoPlayerMode,
+			});
 
-		await expect(setting.input).toBeChecked();
+			const setting = await getAlwaysDisplayPlayerBar();
+			await setting.inputParent.click();
+			await expect(setting.input).toBeChecked({ checked: false });
 
-		const { playVideoPlayer, getPlayerBar } = await openYouTube(page, {
-			url: URLS.sampleVideo,
-		});
+			const { playVideoPlayer, getPlayerBar, getTargetVideoPlayerModeButton } =
+				await openYouTube(page, {
+					url: URLS.sampleVideo,
+				});
 
-		await playVideoPlayer();
+			await playVideoPlayer();
 
-		await waitForCumulativeLayoutShift(page);
+			await waitForCumulativeLayoutShift(page);
 
-		await waitForElementAfterTimeout({
-			page,
-			locator: getPlayerBar(),
-			expect,
-			expectType: "visible",
-			timeout: 5000,
+			const videoPlayerModeButton =
+				await getTargetVideoPlayerModeButton(videoPlayerMode);
+			await videoPlayerModeButton?.click();
+
+			await waitForElementAfterTimeout({
+				page,
+				locator: getPlayerBar(),
+				expect,
+				expectType: "hidden",
+				timeout: 5000,
+			});
 		});
 	});
-
-	test("disable: alwaysDisplayPlayerBar", async ({ page, extensionId }) => {
-		const { getAlwaysDisplayPlayerBar } = await openOptions(page, {
-			extensionId,
-			videoPlayerMode,
-		});
-
-		const setting = await getAlwaysDisplayPlayerBar();
-		await setting.inputParent.click();
-		await expect(setting.input).toBeChecked({ checked: false });
-
-		const { playVideoPlayer, getPlayerBar } = await openYouTube(page, {
-			url: URLS.sampleVideo,
-		});
-
-		await playVideoPlayer();
-
-		await waitForCumulativeLayoutShift(page);
-
-		await waitForElementAfterTimeout({
-			page,
-			locator: getPlayerBar(),
-			expect,
-			expectType: "hidden",
-			timeout: 5000,
-		});
-	});
-});
+}
