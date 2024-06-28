187334f0a1172a1ee85fbcf0d591d19e6c03112c
1natsu <1natsu172@users.noreply.github.com>
2024-06-28 20:47:18 +0900

    feat: setup send sourcemaps for sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import postcssImport from "postcss-import";
 import postcssNesting from "postcss-nesting";
@@ -8,7 +9,20 @@ import reactSvgr from "vite-plugin-svgr";
 import type { WxtViteConfig } from "wxt";
 
 export default {
-	plugins: [react(), reactSvgr(), preserveDirectives()],
+	build: {
+		sourcemap: true, // Source map generation must be turned on (for Sentry)
+	},
+	plugins: [
+		react(),
+		reactSvgr(),
+		preserveDirectives(),
+		//NOTE: For Source Maps - Need put the Sentry vite plugin after all other plugins.
+		sentryVitePlugin({
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+			org: "4d5e926a0e43",
+			project: "oypb",
+		}),
+	],
 	css: {
 		postcss: {
 			plugins: [postcssNesting, postcssImport()],
