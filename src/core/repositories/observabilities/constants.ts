47d98c800310e360374086025af391ebe1f789fc
1natsu <1natsu172@users.noreply.github.com>
2024-07-25 16:31:48 +0900

    chore: tuning sentry capture exceptions.

--- src/core/repositories/observabilities/constants.ts
+++ src/core/repositories/observabilities/constants.ts
@@ -1,3 +1,8 @@
 // NOTE: https://docs.sentry.io/concepts/key-terms/dsn-explainer/
 export const SENTRY_PUB_DSN =
 	"https://fa7b309d5b300fb0d1b4167b271205b7@o163106.ingest.us.sentry.io/4507373163577344";
+
+export const ignoreErrors: (string | RegExp)[] = [
+	"Extension context invalidated",
+	"ResizeObserver loop completed with undelivered notifications",
+];
