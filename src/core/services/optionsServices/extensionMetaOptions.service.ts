e581d908d76b543e629e882d3a60c612965f8bae
1natsu <1natsu172@users.noreply.github.com>
2024-07-30 13:12:31 +0900

    fix: #131 init debugmode issue in the PROD (#134)

--- src/core/services/optionsServices/extensionMetaOptions.service.ts
+++ src/core/services/optionsServices/extensionMetaOptions.service.ts
@@ -1,8 +1,13 @@
+import { getDebugModeOption } from "@/core/presenters/storagePresenter/options.presenter.js";
 import * as repo from "@/core/repositories/options.repository.js";
 import * as usecases from "@/core/usecases/options.usecase.js";
 import { reCreateLoggerInstance } from "@/utils/logger.js";
 
 export const initializeDebugMode = async () => {
+	const isEnabledDebugByOption = await getDebugModeOption();
+	if (isEnabledDebugByOption) {
+		reCreateLoggerInstance({ isDebug: true });
+	}
 	// Support change option reacted.
 	return repo.debugMode.watch((current, prev) => {
 		if (current !== prev) {
