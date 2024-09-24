8e02a1d7b481da05e16709c17929d7ab4cde65f4
1natsu <1natsu172@users.noreply.github.com>
2024-09-25 03:32:36 +0900

    chore: remove unnecessary ts syntax

--- src/sharedUI/Components/forms/SettingsForm/FormDefinition/ExtensionBehaviorForm/InheritPositionPlayerBarBeforeSwitchingField/fieldLibs.ts
+++ src/sharedUI/Components/forms/SettingsForm/FormDefinition/ExtensionBehaviorForm/InheritPositionPlayerBarBeforeSwitchingField/fieldLibs.ts
@@ -8,9 +8,7 @@ export function createInheritablePositionPlayerBarSelect(
 	const dataset = createInheritablePositionPlayerBarData(mode);
 
 	const items = dataset.map<ComboboxData[number]>((modeKey) => {
-		// @ts-expect-error
 		const from = browser.i18n.getMessage(`metawords_${modeKey}`);
-		// @ts-expect-error
 		const to = browser.i18n.getMessage(`metawords_${mode}`);
 		return {
 			label: `${from} → ${to}`,
