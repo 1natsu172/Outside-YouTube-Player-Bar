4cf3bbdda27c62374b14121b9b451bf3f0f9d942
1natsu <1natsu172@users.noreply.github.com>
2024-12-07 21:27:48 +0900

    chore: ignore cdd.d.ts.map

--- src/entrypoints/options/App/components/Footer/Footer.module.css.d.ts
+++ src/entrypoints/options/App/components/Footer/Footer.module.css.d.ts
@@ -2,7 +2,6 @@ declare const styles:
   & Readonly<{ "footer": string }>
   & Readonly<{ "inner": string }>
   & Readonly<{ "logo": string }>
-  & Readonly<{ "links": string }>
 ;
 export default styles;
 //# sourceMappingURL=./Footer.module.css.d.ts.map
