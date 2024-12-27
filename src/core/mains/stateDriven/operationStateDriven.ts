f25699fbe8d157162d2a784eae2b13e67146b12f
1natsu <1natsu172@users.noreply.github.com>
2024-12-28 01:01:15 +0900

    fix: #371 Ensure that you wait for the mainworld to be ready during the reflect phase

--- src/core/mains/stateDriven/operationStateDriven.ts
+++ src/core/mains/stateDriven/operationStateDriven.ts
@@ -1,3 +1,4 @@
+import { getFlagOps } from "@/core/presenters/statePresenter/operationState/index.js";
 import { operationState } from "@/core/repositories/contentScript.repository.js";
 import { domAffectOypbEnable } from "@/core/services/domAffectServices/domMetaAffect.service.js";
 import { subscribeKey } from "valtio/utils";
@@ -8,3 +9,33 @@ export const oypbEnableDriven = () => {
 		domAffectOypbEnable(value);
 	});
 };
+
+export function waitMainWorldReady() {
+	const { promise, resolve } = Promise.withResolvers<void>();
+	const unsubscribe = subscribeKey(
+		operationState.flagOps,
+		"mainWorld",
+		({ scriptReady: isReady }) => {
+			if (isReady) {
+				logger.debug("MainWorld standby");
+				unsubscribe();
+				resolve();
+			}
+		},
+	);
+
+	const {
+		mainWorld: { scriptReady: isReady },
+	} = getFlagOps();
+
+	if (isReady) {
+		unsubscribe();
+		setTimeout(() => {
+			logger.debug("MainWorld already standby.");
+			resolve();
+		}, 0);
+	}
+
+	logger.debug("waiting MainWorld ready…");
+	return promise;
+}
