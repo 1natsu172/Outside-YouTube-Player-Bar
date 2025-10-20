3e28c20fd748dfb10b8151157f37e51f473a7d86
1natsu <1natsu172@users.noreply.github.com>
2025-10-20 22:29:25 +0900

    chore: generate css typings

--- src/entrypoints/youtube.content/UI/components/parts/SettingsButton/style.module.css.d.ts
+++ src/entrypoints/youtube.content/UI/components/parts/SettingsButton/style.module.css.d.ts
@@ -1,6 +1,8 @@
-declare const styles: Readonly<{ button: string }> &
-	Readonly<{ tooltip: string }> &
-	Readonly<{ button: string }> &
-	Readonly<{ tooltip: string }>;
+declare const styles:
+  & Readonly<{ "button": string }>
+  & Readonly<{ "tooltip": string }>
+  & Readonly<{ "button": string }>
+  & Readonly<{ "tooltip": string }>
+;
 export default styles;
 //# sourceMappingURL=./style.module.css.d.ts.map
